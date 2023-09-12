f92aa4c25d5160d2dae3e5e0bc4e40a5c0b06daa
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2023-09-12 16:35:57 -0500

    Add project files.

---
+++ Event/ConsoleHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event
+{
+    public static class ConsoleHelpers
+    {
+        public static void PrintToConsole(this string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Event/Program.cs
@@ -0,0 +1,32 @@
+﻿using Event;
+
+CollegeClassModel history = new CollegeClassModel("History 101", 3);
+CollegeClassModel math = new CollegeClassModel("Calculus 201", 2);
+
+history.EnrollmentFull += CollegeClass_EnrollmentFull;
+
+
+history.SignUpStudent("Tad Pole").PrintToConsole();
+history.SignUpStudent("Joy Pole").PrintToConsole();
+history.SignUpStudent("Laluu Hu").PrintToConsole();
+history.SignUpStudent("Lisa Lao").PrintToConsole();
+history.SignUpStudent("J Wen").PrintToConsole();
+Console.WriteLine();
+
+math.EnrollmentFull += CollegeClass_EnrollmentFull;
+
+void CollegeClass_EnrollmentFull(object? sender, string e)
+{
+    CollegeClassModel model = (CollegeClassModel)sender;
+    Console.WriteLine();
+    Console.WriteLine($"{model.CourseTitle}: Full");
+    Console.WriteLine();
+}
+
+math.SignUpStudent("Laluu Hu").PrintToConsole();
+math.SignUpStudent("Lisa Lao").PrintToConsole();
+math.SignUpStudent("J Wen").PrintToConsole();
+
+
+
+Console.ReadLine();
